e093d8f976f457a6db5d4d91f87918f92b95effa
Will V <vernondesign@proton.me>
2023-04-01 14:09:24 -0400

    added - github and aut0 but github doesnt work as much

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import Auth0Provider from "next-auth/providers/auth0";
+
 import { getUser } from "../../../lib/server";
 import { User } from "../../../types";
 
@@ -30,56 +32,16 @@ export const authOptions = {
   // Configure one or more authentication providers
   // More info: https://next-auth.js.org/providers/
   providers: [
-    // CredentialsProvider is used for the demo auth system
-    // Replace this with a real provider, e.g. GitHub, Auth0
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "email",
-          type: "text",
-        },
-      },
-      async authorize(credentials) {
-        if (!credentials) {
-          return null;
-        }
-
-        const user: User | null = await getUser(credentials.email);
-
-        if (!user) {
-          throw new Error("User not found");
-        }
-
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.id,
-          image: user.avatar,
-        };
-      },
-    }),
-
-    /*
     // Use GitHub authentication
-    // import GithubProvider from "next-auth/providers/github";
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
-    */
-
-    /*
-    // Use Auth0 authentication
-    // import Auth0Provider from "next-auth/providers/auth0";
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID as string,
       clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-      issuer: process.env.AUTH0_ISSUER_BASE_URL,
+      issuer: process.env.AUTH0_ISSUER_BASE_URL as string,
     }),
-    */
-
-    // ...add more providers here
   ],
 };
 
